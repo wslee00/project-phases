42ac8f67490de5bed22a392cd221e479e56c6bf3
William Lee <will.maps@gmail.com>
2025-01-04 02:39:18 -0500

    Add sample data for project phases editor

--- force-app/main/default/lwc/projectPhasesEditor/projectPhasesEditor.js
+++ force-app/main/default/lwc/projectPhasesEditor/projectPhasesEditor.js
@@ -5,6 +5,27 @@ import PROJECT_NAME_FIELD from '@salesforce/schema/Project__c.Name';
 export default class ProjectPhasesEditor extends LightningElement {
     @api recordId;
 
+    phases = [
+        {
+            name: 'Phase 1',
+            duration: 10,
+            startDate: '2025-01-01',
+            endDate: '2025-01-10',
+        },
+        {
+            name: 'Phase 2',
+            duration: 10,
+            startDate: '2025-01-11',
+            endDate: '2025-01-20',
+        },
+        {
+            name: 'Phase 3',
+            duration: 10,
+            startDate: '2025-01-21',
+            endDate: '2025-01-30',
+        },
+    ];
+
     @wire(getRecord, { recordId: '$recordId', fields: [PROJECT_NAME_FIELD] })
     project;
 }
