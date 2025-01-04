d4c34aec9818b1da802867fc7339f638f1eb1845
William Lee <will.maps@gmail.com>
2025-01-04 03:11:03 -0500

    calculate duration based on date changes

--- force-app/main/default/lwc/projectPhaseRowEdit/projectPhaseRowEdit.js
+++ force-app/main/default/lwc/projectPhaseRowEdit/projectPhaseRowEdit.js
@@ -20,15 +20,36 @@ export default class ProjectPhaseRowEdit extends LightningElement {
     }
 
     handleStartDateChange(event) {
-        this.phase.startDate = event.detail.value;
+        this._handleDateChange({ startDate: event.detail.value });
     }
 
     handleEndDateChange(event) {
-        this.phase.endDate = event.detail.value;
+        this._handleDateChange({ endDate: event.detail.value });
     }
 
-    _addToDate(date, days) {
-        let result = new Date(date);
+    _handleDateChange(changes) {
+        const startDateString = changes.startDate || this.phase.startDate;
+        const endDateString = changes.endDate || this.phase.endDate;
+        if (!startDateString || !endDateString) {
+            return;
+        }
+        let startDate = new Date(startDateString);
+        let endDate = new Date(endDateString);
+        const duration = Math.round((endDate - startDate) / (1000 * 60 * 60 * 24));
+        this.dispatchEvent(
+            new CustomEvent('phasechange', {
+                detail: {
+                    ...this.phase,
+                    duration,
+                    startDate: startDateString,
+                    endDate: endDateString,
+                },
+            }),
+        );
+    }
+
+    _addToDate(dateString, days) {
+        let result = new Date(dateString);
         result.setDate(result.getDate() + days + 1);
 
         const offset = result.getTimezoneOffset();
